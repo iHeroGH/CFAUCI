b49b426266ba00258f1340a70cdf194e8993ac90
George Matta <jojomedhat2004@gmail.com>
2022-04-24 23:35:15 -0700

    lightly steal some code

--- cogs/GmailConnection/requester.py
+++ cogs/GmailConnection/requester.py
@@ -22,4 +22,31 @@ class Requester:
     def get_inbox(self):
         self.mail.select('inbox')
 
+    def get_messages(self):
+        # The thrown var is a status check
+        # Search data is a list of bytes
+        _, search_data = self.mail.search(None, 'FROM', '"jojomedhat2004@gmail.com"')
+
+        all_messages = []
+        for message_num in search_data[0].split():
+            email_data = {}
+
+            _, message_data = self.mail.fetch(message_num, '(RFC822)')
+            _, message_bytes = message_data[0]
+            message: email._MessageT = email.message_from_bytes(message_bytes)
+
+            for header in ['to', 'from', 'date', 'subject']:
+                email_data[header] = message[header]
+
+            for part in message.walk():
+                content_type = part.get_content_type()
+
+                if content_type == "text/plain":
+                    body = part.get_payload(decode=True)
+                    email_data['body'] = body.decode()
+
+            all_messages.append(email_data)
+
+        return all_messages
+
 Requester()
