b7eea8030bb73b64c71dc282e15a12904b417b51
George Matta <jojomedhat2004@gmail.com>
2022-04-24 23:00:12 -0700

    fix login

--- cogs/GmailConnection/requester.py
+++ cogs/GmailConnection/requester.py
@@ -1,18 +1,25 @@
 import imaplib
 import email
 
-host = 'imap.gmail.com'
+import os
+import json
+
 
 class Requester:
 
     def __init__(self):
+        self.username, self.password = self.__init_creds__()
         self.__login__()
 
+    def __init_creds__(self):
+        cred_json = json.load(open('config/google_config/credentials.json'))
+        return cred_json['login']['username'], cred_json['login']['password']
 
     def __login__(self):
-        self.mail = imaplib.IMAP4_SSL(host)
-        self.mail.login('username', 'password')
+        self.mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        self.mail.login(self.username, self.password)
 
     def get_inbox(self):
         self.mail.select('inbox')
 
+Requester()
