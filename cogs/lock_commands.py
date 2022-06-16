b020f0fedc70038e2566fcda374cf92246eca9c7
George Matta <jojomedhat2004@gmail.com>
2022-06-15 17:24:57 -0700

    added some logging for the task

--- cogs/lock_commands.py
+++ cogs/lock_commands.py
@@ -1,22 +1,60 @@
-from discord.ext import commands, vbu
+from discord.ext import commands, vbu, tasks
 import discord
+
+import datetime as dt
+import pytz
 
 class LockCommands(vbu.Cog):
 
+    # TASK CALCULATOR
+    TIME_TO_WAIT = 0.30 # in hours
+
+    # Start task when cog is loaded
+    def __init__(self, bot: vbu.Bot):
+        super().__init__(bot)
+        self.check_cat_lock.start()
+
+    # The task itself
+    @tasks.loop(seconds=TIME_TO_WAIT * 3600)
+    async def check_cat_lock(self):
+        """
+        Check if it is a Sunday
+        """
+        if self.is_sunday():
+            self.logger.info("Sunday detected")
+            await self.lock_work_channels()
+        else:
+            self.logger.info("Sunday not detected")
+            await self.unlock_work_channels()
+
+    # Helpers
+    def is_sunday(self):
+        """
+        Checks if it is a Sunday
+        """
+        pst = pytz.timezone("US/Pacific")
+        now = dt.datetime.now(pst)
+
+        return now.weekday() == 6 and now.hour >= 3
+
+    # Stock task when cog is unloaded
+    def cog_unload(self):
+        self.check_cat_lock.cancel()
+
     # PERMISSIONS EDITOR
-    async def edit_work_perms(self, ctx: vbu.Context, perms: discord.PermissionOverwrite = None):
+    async def edit_work_perms(self, perms: discord.PermissionOverwrite = None):
         """
         Edits the permissions of the work category
         """
-
+        self.bot.logger.info("Editing work category permissions to: " + str(perms))
         # We don't want to set the perm to True, just None or False
         if perms:
             perms = None
 
         # Set up our vars
-        gen = self.bot.get_channel(914719765875019786) # The general-work channel
-        cat = gen.category # The entire work category
-        def_role = ctx.guild.default_role # The 'everyone' role
+        chan = self.bot.get_channel(914719765875019786) # The general-work channel
+        cat = chan.category # The entire work category
+        def_role = chan.guild.default_role # The 'everyone' role
 
         # Set our perm overwrite
         overwrite = discord.PermissionOverwrite()
@@ -29,18 +67,22 @@ class LockCommands(vbu.Cog):
         else:
             await cat.edit(name = "Work (Sunday Mode)")
 
+        self.bot.logger.info("Edited work category permissions to: " + str(perms))
+
     # DELEGATOR METHODS
-    async def lock_work_channels(self, ctx):
+    async def lock_work_channels(self):
         """
         Locks the work channels
         """
-        await self.edit_work_perms(ctx, False)
+        self.bot.logger.info("Locking work channels")
+        await self.edit_work_perms(False)
 
-    async def unlock_work_channels(self, ctx):
+    async def unlock_work_channels(self):
         """
         Unlocks the work channels
         """
-        await self.edit_work_perms(ctx)
+        self.bot.logger.info("Unlocking work channels")
+        await self.edit_work_perms()
 
     # COMMANDS
     @commands.command()
@@ -50,7 +92,8 @@ class LockCommands(vbu.Cog):
         Locks all the channels in the work category.
         """
 
-        await self.lock_work_channels(ctx)
+        self.bot.logger.info("Locking work channels manually")
+        await self.lock_work_channels()
         await ctx.okay()
 
     @commands.command()
@@ -60,7 +103,8 @@ class LockCommands(vbu.Cog):
         Unlocks all the channels in the work category.
         """
 
-        await self.unlock_work_channels(ctx)
+        self.bot.logger.info("Unlocking work channels manually")
+        await self.unlock_work_channels()
         await ctx.okay()
 
 
