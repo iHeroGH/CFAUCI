e4e892ad6997cd58431ccf80f673e3e72235cc64
George Matta <jojomedhat2004@gmail.com>
2022-06-27 16:10:12 -0700

    add force weekly emali checks

--- cogs/gmail_commands.py
+++ cogs/gmail_commands.py
@@ -1,4 +1,5 @@
 from discord.ext import commands, vbu
+import discord
 
 from cogs.GmailConnection.requester import Requester
 from cogs.bot_start import BotStart
@@ -7,16 +8,28 @@ class GmailCommands(vbu.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_guild=True)
-    async def force_get_messages(self, ctx: vbu.Context):
+    async def force_get_messages(self, ctx: vbu.Context, channel: discord.TextChannel = None):
         """
         Forces the bot to read all messages from the inbox
         """
         messages = self.bot.requester.get_messages()
+        channel = channel or ctx.channel
 
         for message in messages:
             embed = BotStart.create_embed(message)
-            await ctx.send(embed=embed)
+            await channel.send(embed=embed)
 
+        await ctx.okay()
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def force_weekly_announcement(self, ctx: vbu.Context, channel: discord.TextChannel = None):
+        """
+        Forces the bot to send the weekly email announcement message
+        """
+        channel = channel or ctx.channel
+
+        await channel.send("Weekly emails are being sent out! Make sure to check your inbox for your trainer's email.")
         await ctx.okay()
 
 def setup(bot: vbu.Bot):
