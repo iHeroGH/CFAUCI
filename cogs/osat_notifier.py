949d9b6ad33c6219151780cfdf145cb5dfd96bb9
George Matta <jojomedhat2004@gmail.com>
2022-08-03 22:20:30 -0700

    temp change announcement channel

--- cogs/osat_notifier.py
+++ cogs/osat_notifier.py
@@ -10,7 +10,7 @@ import re
 class OsatNotifier(vbu.Cog):
 
     TIME_TO_WAIT = 0.25 # in hours
-    CHANNEL_ID = 913280556169592893 # Announcements
+    CHANNEL_ID = 913280990145830922 # Bot Coms
 
     def __init__(self, bot: vbu.Bot):
         super().__init__(bot)
