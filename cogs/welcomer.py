c95d1cf3d1fa27272f5b782278ef29ea597de999
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-03-05 22:05:33 -0800

    actually send the embed

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -22,16 +22,18 @@ class Welcomer(vbu.Cog):
 
     async def send_welcome_message(self, member: discord.Member):
         self.bot.logger.info(f"Sending welcome message to {member.id}")
-        await member.send(self.get_welcome_message()) 
+        await member.send(embed=self.get_welcome_message()) 
 
     @staticmethod
     def get_welcome_message() -> str:
-        embed = vbu.Embed(title = "Welcome to Chick-Fil-A!")
+        embed = vbu.Embed(title = "Welcome to Chick-Fil-A!", use_random_colour=True)
         embed.description = ("Hello, hello! Welcome to the team and to the *Unofficial* CFA UCI Discord server!\n\n"
                             "As of right now you aren't able to see much of the server but we can fix that in a jiffy :D"
                             "**Just go ahead and make sure to grab you roles in the <#913262423585202278> channel on the server.**\n\n"
                             "Once you do so, you should have access to the rest of the server!"
                             "Make sure you also read the <#913258973057126440> of the server so you don't get banned ;)\n\n Thanks for joining us for the ride <3")
+
+        return embed
 
 def setup(bot: vbu.Bot):
     x = Welcomer(bot)
