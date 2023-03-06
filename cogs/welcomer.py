33a5713595ee1441c8e4a66a196755270be1b9cd
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-03-05 22:05:00 -0800

    replace with actual welcome message

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -9,6 +9,7 @@ class Welcomer(vbu.Cog):
         await self.send_welcome_message(member)
 
     @commands.command()
+    @commands.has_permissions(manage_guild=True)
     async def force_welcome(self, ctx: vbu.Context, member: discord.Member = None):
         """
         Forcefully sends the welcome message to the user
@@ -25,7 +26,12 @@ class Welcomer(vbu.Cog):
 
     @staticmethod
     def get_welcome_message() -> str:
-        return "Welcome to CFA discord server blah blah"
+        embed = vbu.Embed(title = "Welcome to Chick-Fil-A!")
+        embed.description = ("Hello, hello! Welcome to the team and to the *Unofficial* CFA UCI Discord server!\n\n"
+                            "As of right now you aren't able to see much of the server but we can fix that in a jiffy :D"
+                            "**Just go ahead and make sure to grab you roles in the <#913262423585202278> channel on the server.**\n\n"
+                            "Once you do so, you should have access to the rest of the server!"
+                            "Make sure you also read the <#913258973057126440> of the server so you don't get banned ;)\n\n Thanks for joining us for the ride <3")
 
 def setup(bot: vbu.Bot):
     x = Welcomer(bot)
