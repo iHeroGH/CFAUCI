ac2dc8df360a956e1bb65662b5c1ee3bb576e5a2
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-05-21 15:00:01 -0700

    try making a requester on connect, not ready

--- cogs/bot_start.py
+++ cogs/bot_start.py
@@ -4,7 +4,7 @@ from cogs.GmailConnection.requester import Requester
 class BotStart(vbu.Cog):
 
     @vbu.Cog.listener()
-    async def on_ready(self):
+    async def on_connect(self):
         """
         Create a new requester object when the bot starts
         """
